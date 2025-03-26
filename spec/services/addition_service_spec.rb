11aa6f1f59cd3ef862a52f7af5c35660864f3358
Kishor Vyavahare <vyavahare.kishor@gmail.com>
2025-03-26 16:33:41 +0530

    Refactor the spec code

--- spec/services/addition_service_spec.rb
+++ spec/services/addition_service_spec.rb
@@ -2,25 +2,45 @@ require 'rspec'
 require_relative '../../services/addition_service'
 
 RSpec.describe AdditionService do
-  describe 'add numbers of string format' do
-    it 'should return 0 for an empty string' do
-      expect(AdditionService.new("").call).to eq(0)
+  subject { described_class.new(numbers).call }
+
+  describe 'add numbers present in single string' do
+
+    context 'when string is empty' do
+      let(:numbers) { "" }
+      it 'should return 0' do
+        expect(subject).to eq(0)
+      end
     end
 
-    it 'should return single number for a string containing single number' do
-      expect(AdditionService.new("1").call).to eq(1)
-    end
+    context 'when string is not empty' do
+      context 'when string contain a single number' do
+        let(:numbers) { "1" }
+        it 'should return number itself' do
+          expect(subject).to eq(1)
+        end
+      end
 
-    it 'should return sum of two numbers for string' do
-      expect(AdditionService.new("2,3").call).to eq(5)
-    end
+      context 'when string contain n numbers separated by comma char' do
+        let(:numbers) { "1,2,3,4,5,6,7,8,9" }
+        it 'should return sum of n numbers' do
+          expect(subject).to eq(45)
+        end
+      end
 
-    it 'should return sum of n numbers for string' do
-      expect(AdditionService.new("1,2,3,4,5,6,7,8,9").call).to eq(45)
-    end
+      context 'when string contain n numbers separated by new line char' do
+        let(:numbers) { "1\n2\n3\n4" }
+        it 'should return sum of n numbers' do
+          expect(subject).to eq(10)
+        end
+      end
 
-    it 'should return sum of n numbers for string separated by comma and newline' do
-      expect(AdditionService.new("1,2\n3,4").call).to eq(10)
+      context 'when string contain n numbers separated by comma and new line char' do
+        let(:numbers) { "1,2,3\n4,5,6\n7,8,9" }
+        it 'should return sum of n numbers' do
+          expect(subject).to eq(45)
+        end
+      end
     end
   end
 end
