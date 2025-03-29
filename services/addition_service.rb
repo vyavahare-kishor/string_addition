062a6425349f1e20b8ae756dd96601dc431c8c24
Kishor Vyavahare <vyavahare.kishor@gmail.com>
2025-03-29 12:46:34 +0530

    Refactor the code

--- services/addition_service.rb
+++ services/addition_service.rb
@@ -8,18 +8,27 @@ class AdditionService
   def call
     return 0 if numbers.nil? || numbers.empty?
 
+    delimiter = find_delimiter_and_numbers
+    @numbers = numbers.split(/#{delimiter}/).map(&:to_i)
+    raise_if_numbers_negative
+    numbers.reject { |n| n > 1000 }.sum
+  end
+
+  private
+
+  def find_delimiter_and_numbers
     delimiter = ",|\n"
-    if numbers.start_with?("//")
-      delimiter_part, @numbers = numbers.split("\n", 2)
+    if @numbers.start_with?("//")
+      delimiter_part, @numbers = @numbers.split("\n", 2)
       delimiter = delimiter_part[2..-1]
     end
+    delimiter
+  end
 
-    @numbers = numbers.split(/#{delimiter}/).map(&:to_i)
-    negative_set = numbers.select(&:negative?)
+  def raise_if_numbers_negative
+    negative_set = @numbers.select(&:negative?)
     unless negative_set.empty?
       raise "negative numbers not allowed #{negative_set.join(',')}"
     end
-
-    numbers.reject { |n| n > 1000 }.sum
   end
 end
